6e4f4e20973c4b0d4d6fd7b6e94d6314da01c7c6
penerus programmer <engkoskostaman759@gmail.com>
2022-08-09 21:54:08 +0700

    skripsi projectku
    modal ATM

---
+++ includes/footer.php
@@ -0,0 +1,38 @@
+
+
+<footer>
+  <div class="footer-top">
+    <div class="container">
+      <div class="row">
+      
+        <div class="col-md-6">
+          <ul>
+          <li><a href="page.php?type=aboutus">About Us</a></li>
+            <li><a href="page.php?type=baju">Merchandise</a></li>
+          <li><a href="page.php?type=panduan">Panduan Pembayaran</a></li>
+          </ul>
+        </div>
+      </div>
+    </div>
+  </div>
+
+  <div class="footer-bottom">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-6 col-md-push-6 text-right">
+          <div class="footer_widget">
+            <p>Link Sosial Media :</p>
+            <ul>
+              <li><a href="https://www.facebook.com/engkosaelh/"><i class="fa fa-facebook-square" aria-hidden="true"></i></a></li>
+              <li><a href="https://www.instagram.com/cekosgembelelite/"><i class="fa fa-instagram" aria-hidden="true"></i></a></li>
+            </ul>
+          </div>
+        </div>
+        <div class="col-md-6 col-md-pull-6">
+          <p class="copy-right">Copyright &copy; ENGKOS KOSTAMAN -STMIK INSAN PEMBANGUNAN
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</footer>
